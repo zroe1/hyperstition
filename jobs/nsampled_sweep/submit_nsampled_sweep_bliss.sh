1488ba3eb5236a5311e9ecbf3d42317924551ab7
Jack Sanderson <jackmsanderson@gmail.com>
2026-05-23 18:18:00 -0500

    mass queue

---
+++ jobs/nsampled_sweep/submit_nsampled_sweep_bliss.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Submit all bliss nsampled-sweep training jobs, then eval jobs dependent on each.
+
+for NTE in 100 500 1000 2000; do
+  TRAIN_SCRIPT="jobs/nsampled_sweep/nsampled_sweep_bliss_4b_seed16_nte${NTE}.sh"
+  EVAL_SCRIPT="jobs/nsampled_sweep/eval_nsampled_sweep_bliss_4b_seed16_nte${NTE}.sh"
+
+  TRAIN=$(sbatch --parsable "$TRAIN_SCRIPT")
+  echo "Submitted ${TRAIN_SCRIPT}: $TRAIN"
+  EVAL=$(sbatch --parsable --dependency=afterok:$TRAIN "$EVAL_SCRIPT")
+  echo "Submitted ${EVAL_SCRIPT}: $EVAL (after $TRAIN)"
+done
---
+++ jobs/nsampled_sweep/submit_nsampled_sweep_sycophancy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Submit all sycophancy nsampled-sweep training jobs, then eval jobs dependent on each.
+
+for NTE in 100 500 1000 2000; do
+  TRAIN_SCRIPT="jobs/nsampled_sweep/nsampled_sweep_sycophancy_4b_seed18_nte${NTE}.sh"
+  EVAL_SCRIPT="jobs/nsampled_sweep/eval_nsampled_sweep_sycophancy_4b_seed18_nte${NTE}.sh"
+
+  TRAIN=$(sbatch --parsable "$TRAIN_SCRIPT")
+  echo "Submitted ${TRAIN_SCRIPT}: $TRAIN"
+  EVAL=$(sbatch --parsable --dependency=afterok:$TRAIN "$EVAL_SCRIPT")
+  echo "Submitted ${EVAL_SCRIPT}: $EVAL (after $TRAIN)"
+done
