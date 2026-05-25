d3a71aae34e2e27283f29bb12836e0a3b8631278
jcksanderson <jackmsanderson@gmail.com>
2026-05-25 10:14:44 -0500

    sync

--- jobs/nsampled_sweep/nsampled_sweep_bliss_4b_seed16_nte50.sh
+++ jobs/nsampled_sweep/nsampled_sweep_bliss_4b_seed16_nte50.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/ns_bliss_4b_seed16_nte50-%j.out
 #SBATCH --error=logs/ns_bliss_4b_seed16_nte50-%j.err
 #SBATCH --partition=fast
-#SBATCH --time=23:59:00
+#SBATCH --time=3:59:00
 #SBATCH --mem=120G
 
 echo "Job started at: $(date)"
